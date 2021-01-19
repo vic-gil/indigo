817c0b346ff0fd21436f0af4c3d363ab2659d565
vic-gil <vicgilroman@gmail.com>
2021-01-19 16:03:25 -0600

    Agregar el archivo de funciones

---
+++ functions.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Reporte Indigo funciones y definiciones
+ *
+ * @package Capital Media
+ * @subpackage Reporte Indigo
+ * @since Reporte Indigo 3.0.0
+ */
+
+/**
+ * Reporte Indigo trabaja únicamente con Wordpress 4.7 o superior.
+ */
+ 
+require get_template_directory() . '/classes/class-reporte-indigo-templates.php';
+require get_template_directory() . '/classes/embed/class-reporte-indigo-oembed.php';
+?>
