4c1be39109c4151af3ff63079919ce0ea3020d17
vic-gil <vicgilroman@gmail.com>
2021-01-19 17:52:50 -0600

    Agregar funciones para pruebas

--- functions.php
+++ functions.php
@@ -10,7 +10,8 @@
 /**
  * Reporte Indigo trabaja únicamente con Wordpress 4.7 o superior.
  */
- 
+
+require get_template_directory() . '/classes/class-reporte-indigo-test.php';
 require get_template_directory() . '/classes/class-reporte-indigo-templates.php';
 require get_template_directory() . '/classes/embed/class-reporte-indigo-oembed.php';
 ?>
