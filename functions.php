85f11c9f3bf25a51698c8f823aff14c798a6072c
vic-gil <vicgilroman@gmail.com>
2021-01-21 19:30:40 -0600

    Añadir funciones para menús dinámicos

--- functions.php
+++ functions.php
@@ -14,4 +14,18 @@
 require get_template_directory() . '/classes/class-reporte-indigo-test.php';
 require get_template_directory() . '/classes/class-reporte-indigo-templates.php';
 require get_template_directory() . '/classes/embed/class-reporte-indigo-oembed.php';
+
+/*
+ * Se crean tres secciones para menús
+ *
+ */
+function reporte_indigo_setup() {
+	register_nav_menus([
+		'header' => __( 'Cabecera', 'reporte_indigo' ),
+		'help' 	 => __( 'Ayuda', 'reporte_indigo' ),
+		'sites'  => __( 'Sitios del grupo', 'reporte_indigo' )
+	]);	
+}
+
+add_action( 'after_setup_theme', 'reporte_indigo_setup' );
 ?>
