1848426fee90998b98612fafa0cf3af1229ec038
vic-gil <vicgilroman@gmail.com>
2021-01-21 19:54:40 -0600

    Se agrega el script loader

--- functions.php
+++ functions.php
@@ -13,18 +13,31 @@
 
 require get_template_directory() . '/classes/class-reporte-indigo-test.php';
 require get_template_directory() . '/classes/class-reporte-indigo-templates.php';
+require get_template_directory() . '/classes/class-reporte-indigo-script-loader.php';
 require get_template_directory() . '/classes/embed/class-reporte-indigo-oembed.php';
 
-/*
- * Se crean tres secciones para menús
- *
- */
 function reporte_indigo_setup() {
+
+	/*
+	 * Agrega tres menús dinamicos accesible por 
+	 * medio del personalizador
+	 *
+	 */
+
 	register_nav_menus([
 		'header' => __( 'Cabecera', 'reporte_indigo' ),
 		'help' 	 => __( 'Ayuda', 'reporte_indigo' ),
 		'sites'  => __( 'Sitios del grupo', 'reporte_indigo' )
-	]);	
+	]);
+
+	/*
+	 * Agrega soporte de `async` y `defer` para scripts 
+	 * registrados o en cola por el tema
+	 *
+	 */
+
+	$loader = new Reporte_indigo_script_loader();
+	add_filter( 'script_loader_tag', [ $loader, 'filter_script_loader_tag' ], 10, 2 );
 }
 
 add_action( 'after_setup_theme', 'reporte_indigo_setup' );
