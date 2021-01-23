15ab098f76b188eb9f83fcd80a5aaa8e1c92ba27
vic-gil <vicgilroman@gmail.com>
2021-01-22 21:54:24 -0600

    Añadir filtro para imágenes
    Se ha añadido una filtro para reescribir atributos de imágenes

--- functions.php
+++ functions.php
@@ -52,4 +52,107 @@ function reporte_indigo_default_scripts(){
 }
 
 add_action( 'wp_footer', 'reporte_indigo_default_scripts' , 5 );
+
+/**
+ * Filtra los atributos de una imagen adjunta
+ *
+ * @param  string[] $attr 	   Matriz de valores de atributo para el marcado de la imagen.
+ * @param  WP_Post $attachment Imagen adjunta a publicación.
+ * @param  string|int[] $size  Tamaño de imagen solicitado
+ */
+
+function custom_media_responsive_size($attr, $attachment, $size) {
+	if(! is_admin() ) :
+
+		/**
+		 * Agregamos la clase lazyload a todas las imágenes
+		 *
+		 */
+		$attr['class'] = $attr['class'] . " lazyload";
+
+		if( ! empty( $attr['src'] ) ) :
+
+			/**
+			 * Pasamos los valores de src a data-src para el plugin
+			 * Si tus imagenes se encuentran en un CDN (Amazon S3, Cludfront) aquí puedes cambiarlas
+			 * mediante una exprresión regular
+			 *
+			 */
+			$attr['data-src'] = $attr['src'];
+
+			/**
+			 * Pixel 1x1 transparente
+			 *
+			 */
+			$attr['src'] = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII=';
+
+		endif;
+
+		if( ! empty( $attr['srcset'] ) ) :
+
+			/**
+			 * Pasamos los valores de srcset a data-srcset para el plugin
+			 * Si tus imagenes se encuentran en un CDN (Amazon S3, Cludfront) aquí puedes cambiarlas
+			 * mediante una exprresión regular
+			 *
+			 */
+			$attr['data-srcset'] = $attr['srcset'];
+
+			/**
+			 * Limpiamos el campo srcset
+			 *
+			 */
+		   	$attr['srcset'] = '';
+
+		endif;
+
+		/**
+		 * ¡LA MAGIA OCURRE AQUÍ!
+		 * Esto requiere de mucho análisis y de requisitos de tu tema
+		 * usa la configuracion que necesites mediante una medida
+		 * ejemplo `medium_large`, `large` y la sección ejemplo
+		 * is_home(), is_category() para mostrar las imagenes en su tamaño 
+		 * ideal mediante diseño responsivo.
+		 *
+		 * Si no estás familiarizado con esto consulta. HTML y AMP
+		 * @link https://developer.mozilla.org/es/docs/Learn/HTML/Multimedia_and_embedding/Responsive_images
+		 * @link https://amp.dev/es/documentation/guides-and-tutorials/develop/style_and_layout/art_direction/
+		 *
+		 */
+		$attr['sizes'] = "(min-width: 1024px) 500px, (min-width: 768px) 300px, (min-width: 414px) 200px, 300px";
+
+		if( is_home() ) :
+			
+			if( $size === "medium_large" ) :
+				$attr['sizes'] = "(min-width: 1024px) 500px, (min-width: 768px) 300px, (min-width: 414px) 200px, 300px";
+			endif;
+
+			if( $size === "large" ) :
+				$attr['sizes'] = "(min-width: 1024px) 500px, (min-width: 768px) 300px, (min-width: 414px) 200px, 300px";
+			endif;
+
+		endif;
+
+		if ( is_category() ) :
+
+		endif;
+
+		if ( is_tax() ) :
+
+		endif;
+
+		if ( is_search() ) :
+
+		endif;
+
+		if ( is_author() ) :
+
+		endif;
+
+	endif;
+
+	return $attr;
+}
+
+add_filter('wp_get_attachment_image_attributes', 'custom_media_responsive_size', 10 , 3);
 ?>
