7a2169cb869d3a31fa6e5cde8e4ade8a67e3187e
vic-gil <vicgilroman@gmail.com>
2021-01-25 21:30:25 -0600

    Actualizar template wpp_custom_html

--- functions.php
+++ functions.php
@@ -165,30 +165,34 @@ function reporte_indigo_popular_posts_html_list($popular_posts, $instance){
     	$url = str_replace(get_site_url(), 'https://images.reporteindigo.com', $url);
     	$tema = get_the_terms($post_id, 'ri-tema');
 
-        $output .= '<div class="component-lista-imagen">';
-	$output .= '	<article itemtype="http://schema.org/Article">';
-	$output .= '		<figure itemprop="image" itemscope="" itemtype="http://schema.org/ImageObject">';
-	$output .= '			<a href="' . get_permalink($post_id) . '" title="' . esc_attr($popular_post->title) . '">';
-	$output .= '				<picture>';
-	$output .= '					<img itemprop="contentUrl" src="data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==" data-src="' . $url . '" alt="' . esc_attr($popular_post->title) . '" title="' . esc_attr($popular_post->title) . '" class="lazy" loading="lazy" />';
-	$output .= '				</picture>';
-	$output .= '			</a>';
-	$output .= '		</figure>';
-	$output .= '		<div class="entry-data">';
-	$output .= '			<div class="entry-title">';
-	$output .= '				<h2>';
-	$output .= '					<a href="' . get_term_link($tema[0]->term_id) . '" title="' . $tema[0]->name . '">';
-	$output .= $tema[0]->name;
-	$output .= '					</a>';
-	$output .= '				</h2>';
-	$output .= '				<h3>';
-	$output .= '					<a href="' . get_permalink($post_id) . '" title="' . esc_attr($popular_post->title) . '">' . esc_attr($popular_post->title) . '</a>';
-	$output .= '				</h3>';
-        $output .= '			</div>';
-        $output .= '		</div>';
-        $output .= '	</article>';
-        $output .= '</div>';
+    	if( $instance['shorten_title']['active'] == 1)
+    		$title = wp_trim_words( $popular_post->title, $instance['shorten_title']['length'], '...' );
 
+		$output .= '<div class="component-lista-imagen">';
+		$output .= '	<article itemtype="http://schema.org/Article">';
+		$output .= '		<figure itemprop="image" itemscope="" itemtype="http://schema.org/ImageObject">';
+		$output .= '			<a href="' . get_permalink($post_id) . '" title="' . esc_attr($popular_post->title) . '">';
+		$output .= '				<picture>';
+		$output .= '					<img itemprop="contentUrl" src="data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==" data-src="' . $url . '" alt="' . esc_attr($popular_post->title) . '" title="' . esc_attr($popular_post->title) . '" class="lazy" loading="lazy" />';
+		$output .= '				</picture>';
+		$output .= '			</a>';
+		$output .= '		</figure>';
+		$output .= '		<div class="entry-data">';
+		$output .= '			<div class="entry-title">';
+		$output .= '				<h2>';
+		$output .= '					<a href="' . get_term_link($tema[0]->term_id) . '" title="' . $tema[0]->name . '">';
+		$output .= $tema[0]->name;
+		$output .= '					</a>';
+		$output .= '				</h2>';
+		$output .= '				<h3>';
+		$output .= '					<a href="' . get_permalink($post_id) . '" title="' . esc_attr($popular_post->title) . '">';
+		$output .= $title;
+		$output .= ' 					</a>';
+		$output .= '				</h3>';
+		$output .= '			</div>';
+		$output .= '		</div>';
+		$output .= '	</article>';
+		$output .= '</div>';
     }
 
     return $output;
