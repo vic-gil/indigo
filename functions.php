b61d1e7959800dad12d32403eb26c1b650344129
vic-gil <vicgilroman@gmail.com>
2021-02-05 13:53:31 -0600

    Se agregan condiciones para hojas de estilo
    Se agregan condicionales para la página de ventas

--- functions.php
+++ functions.php
@@ -50,12 +50,16 @@ add_action( 'after_setup_theme', 'reporte_indigo_setup' );
 function reporte_indigo_scripts(){
 	wp_enqueue_style( 'critical-style', get_stylesheet_directory_uri() . "/css/critical.css", [], "20210120" );
 
-	if( ! is_home() && ! is_single() && ! is_post_type_archive('ri-reporte') && ! is_post_type_archive('ri-latitud') && ! is_post_type_archive('ri-indigonomics') && ! is_post_type_archive('ri-piensa') && ! is_tax('ri-categoria') && ! is_tax('ri-tema') && ! is_tax('ri-columna') && ! is_post_type_archive('ri-fan') && ! is_post_type_archive('ri-desglose')  && ! is_post_type_archive('ri-opinion') && ! is_page_template('page-templates/terminos.php') && ! is_page_template('page-templates/privacidad.php')  ) {
+	if( ! is_home() && ! is_single() && ! is_post_type_archive('ri-reporte') && ! is_post_type_archive('ri-latitud') && ! is_post_type_archive('ri-indigonomics') && ! is_post_type_archive('ri-piensa') && ! is_tax('ri-categoria') && ! is_tax('ri-tema') && ! is_tax('ri-columna') && ! is_post_type_archive('ri-fan') && ! is_post_type_archive('ri-desglose')  && ! is_post_type_archive('ri-opinion') && ! is_page_template('page-templates/terminos.php') && ! is_page_template('page-templates/privacidad.php') && ! is_page_template('page-templates/ventas.php') ) {
 		wp_enqueue_style('bootstrap-min-css', 'https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css', array(), '4.4.1', 'all' );
 		wp_enqueue_style('my-stylesheet-css', get_stylesheet_uri(), array(), '0.0.1', 'all' );
 		wp_enqueue_style('my-768-css', get_template_directory_uri()."/assets/css/768.css", array(), '0.0.1', 'screen and (max-width: 768px)');
 		wp_enqueue_style('my-576-css', get_template_directory_uri()."/assets/css/576.css", array(), '0.0.1', 'screen and (max-width: 576px)');
 		wp_enqueue_style('my-425-css', get_template_directory_uri()."/assets/css/425.css", array(), '0.0.1', 'screen and (max-width: 425px)');
+	}
+	
+	if( is_page_template('page-templates/ventas.php') ) {
+		wp_enqueue_style( 'ventas-style', get_stylesheet_directory_uri() . "/css/ventas.css", [], "20210120" );
 	}
 
 	if( is_page_template('page-templates/terminos.php') || is_page_template('page-templates/privacidad.php') ){
