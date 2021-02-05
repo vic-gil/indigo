d6d53a9e341dd4963aed014c87b2fe3c511eac32
vic-gil <vicgilroman@gmail.com>
2021-02-04 23:44:54 -0600

    Se actualiza el template

--- page-templates/terminos.php
+++ page-templates/terminos.php
@@ -9,12 +9,32 @@
  * @since Reporte Indigo 3.0.0
  */
 
-get_header(); ?>
+get_header();?>
 
 <main>
 	<div class="container wm">
 		<div class="components">
-			
+			<div id="breadcrumb">
+				<span>
+					<a href="<?=home_url()?>" title="Regresar a página de inicio">Inicio</a>
+				</span>
+				<span class="active">
+					<h1>Términos de uso</h1>
+				</span>
+			</div>
+			<div class="col-lg-8 entry-content">
+				<h2>GRUPO CAPITAL MEDIA</h2>
+				<?php
+				if ( have_posts() ): 
+					while ( have_posts() ): the_post();
+						the_content();
+					endwhile;
+				endif;
+				?>
+			</div>
+			<div class="col-lg-4">
+				
+			</div>
 		</div>
 	</div>
 </main>
