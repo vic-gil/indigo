8fdcb528cc3ed7247c2539dcbc990b32769d332c
vic-gil <vicgilroman@gmail.com>
2021-02-05 12:16:31 -0600

    Se agregan condiciones para hojas de estilo
    Se agregaron condiciones para mostrar el estilo en las secciones de términos de uso y privacidad

--- functions.php
+++ functions.php
@@ -50,7 +50,7 @@ add_action( 'after_setup_theme', 'reporte_indigo_setup' );
 function reporte_indigo_scripts(){
 	wp_enqueue_style( 'critical-style', get_stylesheet_directory_uri() . "/css/critical.css", [], "20210120" );
 
-	if( ! is_home() && ! is_single() && ! is_post_type_archive('ri-reporte') && ! is_post_type_archive('ri-latitud') && ! is_post_type_archive('ri-indigonomics') && ! is_post_type_archive('ri-piensa') && ! is_tax('ri-categoria') && ! is_tax('ri-tema') && ! is_tax('ri-columna') && ! is_post_type_archive('ri-fan') && ! is_post_type_archive('ri-desglose')  && ! is_post_type_archive('ri-opinion') && ! is_page_template('page-templates/terminos.php')  ){
+	if( ! is_home() && ! is_single() && ! is_post_type_archive('ri-reporte') && ! is_post_type_archive('ri-latitud') && ! is_post_type_archive('ri-indigonomics') && ! is_post_type_archive('ri-piensa') && ! is_tax('ri-categoria') && ! is_tax('ri-tema') && ! is_tax('ri-columna') && ! is_post_type_archive('ri-fan') && ! is_post_type_archive('ri-desglose')  && ! is_post_type_archive('ri-opinion') && ! is_page_template('page-templates/terminos.php') && ! is_page_template('page-templates/privacidad.php')  ) {
 		wp_enqueue_style('bootstrap-min-css', 'https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css', array(), '4.4.1', 'all' );
 		wp_enqueue_style('my-stylesheet-css', get_stylesheet_uri(), array(), '0.0.1', 'all' );
 		wp_enqueue_style('my-768-css', get_template_directory_uri()."/assets/css/768.css", array(), '0.0.1', 'screen and (max-width: 768px)');
@@ -58,7 +58,7 @@ function reporte_indigo_scripts(){
 		wp_enqueue_style('my-425-css', get_template_directory_uri()."/assets/css/425.css", array(), '0.0.1', 'screen and (max-width: 425px)');
 	}
 
-	if( is_page_template('page-templates/terminos.php') ){
+	if( is_page_template('page-templates/terminos.php') || is_page_template('page-templates/privacidad.php') ){
 		wp_enqueue_style( 'taxonomy-style', get_stylesheet_directory_uri() . "/css/terminos.css", [], "20210120" );
 	}
 
