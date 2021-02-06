0667c4f2ff3e3a836ed670ee7ad35a20aa49b226
vic-gil <vicgilroman@gmail.com>
2021-02-05 19:36:22 -0600

    Se actualiza la edición impresa

--- page-templates/edicion-impresa.php
+++ page-templates/edicion-impresa.php
@@ -14,7 +14,23 @@ get_header(); ?>
 <main>
 	<div class="container wm">
 		<div class="components">
-			
+			<nav aria-label="Breadcrumb" class="breadcrumb">
+				<ol>
+					<li>
+						<a href="<?=home_url()?>" title="Regresar a página de inicio">Inicio</a>
+					</li>
+					<li>
+						<h1>
+							<a href="<?=get_permalink();?>" aria-current="page">
+								<?=get_the_title();?>
+							</a>
+						</h1>
+					</li>
+				</ol>
+			</nav>
+			<section class="digital">
+				<iframe src="https://services.publish88.com/app/newspaper/publicacion-1158" frameborder="0" allowfullscreen></iframe>
+			</section>
 		</div>
 	</div>
 </main>
