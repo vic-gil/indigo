c58cf626987077d51fac1460f7ae25b26beb337d
vic-gil <vicgilroman@gmail.com>
2021-02-11 15:55:20 -0600

    Se cargan  las últimas plantillas

--- template-parts/components/ri-dato_dia.php
+++ template-parts/components/ri-dato_dia.php
@@ -1,20 +1,27 @@
 <?php
 /**
- * Parte de la plantilla para mostrar el componente twitter plus
+ * Parte de la plantilla para mostrar el componente dato del día
  * @package Capital Media
  * @subpackage Reporte Indigo
  * @since Reporte Indigo 3.0.0
  */
 $class = array_key_exists('class', $args) ? $args['class'] : '';
 ?>
-<div class="component-twitt-plus <?=$class;?>">
+<div class="component-dato-dia <?=$class;?>">
 	<div class="entries">
 		<div class="header">
-			<img src="<?=IMAGESPATH;?>/svgs/filosofia-financiera.svg" alt="Filosofía financiera" width="35">
-			<h2>Filosofía financiera</h2>	
+			<h2>Dato del día</h2>
+			<figure>
+				<picture>
+					<?php the_post_thumbnail("large"); ?>
+				</picture>
+			</figure>	
 		</div>
+		<span class="triangle"></span>
 		<div class="body">
-			<h3><?php the_content();?></h3>
+			<div>
+				<h3><?php the_content();?></h3>
+			</div>
 		</div>
 		<div class="footer">
 			<button 
@@ -24,5 +31,4 @@ $class = array_key_exists('class', $args) ? $args['class'] : '';
 				onclick="utilerias.tweet(this);">TWEET</button>
 		</div>
 	</div>
-	<div class="ticket-cut"></div>
 </div>
