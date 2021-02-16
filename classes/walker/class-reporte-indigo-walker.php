9e2d018b3a5cddac99b94ca90bb0a02a9a3c96ac
vic-gil <vicgilroman@gmail.com>
2021-02-15 22:03:42 -0600

    Se actualiza el walker con un valor por defecto

--- classes/walker/class-reporte-indigo-walker.php
+++ classes/walker/class-reporte-indigo-walker.php
@@ -20,19 +20,31 @@ class Reporte_Indigo_Walker extends Walker_Category_Checklist {
 	 * y deshabilitando el editor rápido
 	 *
 	 * @param array $elements  Un arreglo de elementos.
-     * @param int   $max_depth La máxima profundidad jerarquica.
-     * @param mixed ...$args   Argumentos opcionales.
-     *
-     * @return string La salida de la lista jerarquica
+     	 * @param int   $max_depth La máxima profundidad jerarquica.
+     	 * @param mixed ...$args   Argumentos opcionales.
+     	 *
+     	 * @return string La salida de la lista jerarquica
 	**/
 	function walk( $elements, $max_depth, ...$args ) {
 		$output = parent::walk( $elements, $max_depth, ...$args ); // La función walk heredada del objeto Walker
+		
+		$selected = func_get_arg(2); // La segunda función contiene los datos guardados
+		if( empty( $selected['selected_cats'] ) ) { // Revisamos que no tenga un valor
+			$term = get_term_by("slug", "nacional", "ri-ciudad"); // Seleccionamos el termino por defecto
+			$default = $term->term_id;
+			
+			$output = str_replace(
+				['value="' . $default . '"', "value='" . $default . "'"],
+				['value="' . $default . '" checked', "value='" . $default . "' checked"],
+				$output
+			);
+		}
 
 		$output = str_replace(
 			['type="checkbox"', "type='checkbox'"],
 			['type="radio"', "type='radio'"],
-            $output
-        );
+            		$output
+		);
 
 		return $output;
 	}
