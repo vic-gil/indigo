1c624e901745bb2c60571945d3baccf716be693f
vic-gil <vicgilroman@gmail.com>
2021-02-23 22:15:31 -0600

    Actualizar clase

--- inc/perfomance/perfomance-reporte-indigo-posts.php
+++ inc/perfomance/perfomance-reporte-indigo-posts.php
@@ -11,13 +11,16 @@ function reporte_indigo_exclude_posts( $section, $unique = true, $sort = true ) 
 		$option4 = unserialize( get_option("wp_front_home_opinion_ri") );
 		$option5 = unserialize( get_option("wp_front_home_seleccion_editor_ri") );
 
-		$option1 = ( FALSE !== $option1 ) ? $option1 : [];
-		$option2 = ( FALSE !== $option2 ) ? $option2 : [];
-		$option3 = ( FALSE !== $option3 ) ? $option3 : [];
-		$option4 = ( FALSE !== $option4 ) ? $option4 : [];
-		$option5 = ( FALSE !== $option5 ) ? $option5 : [];
+		$option1 = ( FALSE !== $option1 && is_array( $option1 ) ) ? $option1 : [];
+		$option2 = ( FALSE !== $option2 && is_array( $option2 ) ) ? $option2 : [];
+		$option3 = ( FALSE !== $option3 && is_array( $option3 ) ) ? $option3 : [];
+		$option4 = ( FALSE !== $option4 && is_array( $option4 ) ) ? $option4 : [];
+		$option5 = ( FALSE !== $option5 && is_array( $option5 ) ) ? $option5 : [];
 
-		$exclude = array_merge($option1, $option2, $option3['primary'], $option3['secondary'], $option4, $option5);
+		$option3_1 = array_key_exists('primary', $option3) ? $option3['primary'] : [];
+		$option3_2 = array_key_exists('secondary', $option3) ? $option3['secondary'] : [];
+
+		$exclude = array_merge($option1, $option2, $option3_1, $option3_2, $option4, $option5);
 		
 		if($unique)
 			$exclude = array_unique($exclude, SORT_NUMERIC);
