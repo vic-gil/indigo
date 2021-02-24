b4c7513dc90f68b57a95a1d7d80bf992e3209a04
vic-gil <vicgilroman@gmail.com>
2021-02-23 21:38:46 -0600

    Actualizar clase

--- inc/perfomance/perfomance-reporte-indigo-posts.php
+++ inc/perfomance/perfomance-reporte-indigo-posts.php
@@ -11,6 +11,12 @@ function reporte_indigo_exclude_posts( $section, $unique = true, $sort = true ) 
 		$option4 = unserialize( get_option("wp_front_home_opinion_ri") );
 		$option5 = unserialize( get_option("wp_front_home_seleccion_editor_ri") );
 
+		$option1 = ( FALSE !== $option1 ) ? $option1 : [];
+		$option2 = ( FALSE !== $option2 ) ? $option2 : [];
+		$option3 = ( FALSE !== $option3 ) ? $option3 : [];
+		$option4 = ( FALSE !== $option4 ) ? $option4 : [];
+		$option5 = ( FALSE !== $option5 ) ? $option5 : [];
+
 		$exclude = array_merge($option1, $option2, $option3['primary'], $option3['secondary'], $option4, $option5);
 		
 		if($unique)
