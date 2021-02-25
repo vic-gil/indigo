b19b2fe9155da20c40490bfa75dc43114587c4d9
vic-gil <vicgilroman@gmail.com>
2021-02-25 15:33:11 -0600

    actualizar parte de plantilla

--- template-parts/components/ri-lista_especial.php
+++ template-parts/components/ri-lista_especial.php
@@ -9,7 +9,7 @@
 $tema = get_the_terms( get_the_ID(), 'ri-tema' );
 $class = array_key_exists('class', $args) ? $args['class'] : '';
 ?>
-<div class="component-lista-especial <?=$class;?>">
+<div class="component-lista-especial <?=$class;?>" data-link="<?php the_post_thumbnail_url('medium_large');?>" onmouseover="hoverSpecial(this);" onmouseout="originalSpecial(this);">
 	<article itemtype="http://schema.org/Article">
 		<div class="entry-content">
 			<?php
