6cc5b1621d280164b78a2dae6d4108d69b13469a
vic-gil <vicgilroman@gmail.com>
2021-02-25 15:56:20 -0600

    Se actualizan plantillas

--- template-parts/single/ri-featured_image.php
+++ template-parts/single/ri-featured_image.php
@@ -6,17 +6,47 @@
  * @subpackage Reporte Indigo
  * @since Reporte Indigo 3.0.0
  */
+$galeria = get_post_gallery( get_the_ID(), FALSE );
 $jwplayer = get_post_meta( get_the_ID(), '_mediaid_jwp_meta', TRUE );
 $tema = get_the_terms( get_the_ID(), 'ri-tema');
 ?>
-
 <header class="component-cabecera">
 	<div class="components">
+		<?php
+		if ( false !== $galeria ){
+			$imagenes = explode(',', $galeria['ids']);
+		?>
+		<div class="swiper-container" id="deslizador-single">
+			<div class="swiper-wrapper">
+				<?php
+				foreach ($imagenes as $imagen) {
+				?>
+				<div class="swiper-slide" itemprop="associatedMedia" itemscope="" itemtype="http://schema.org/ImageObject">
+					<figure itemprop="image" itemscope="" itemtype="http://schema.org/ImageObject">
+						<picture>
+							<?=wp_get_attachment_image($imagen, 'medium');?>
+						</picture>
+					</figure>
+				</div>
+				<?php
+				}
+				?>
+			</div>
+			<?php
+				Reporte_indigo_templates::componente_boton_deslizamiento();
+			?>
+		</div>
+		<?php
+		} else {
+		?>
 		<figure itemprop="image" itemscope="" itemtype="http://schema.org/ImageObject">
 			<picture>
 				<?php the_post_thumbnail("large"); ?>
 			</picture>
 		</figure>
+		<?php	
+		}
+		?>
 		<div class="entry-data">
 			<?php
 			if( ! empty( $jwplayer ) ) {
