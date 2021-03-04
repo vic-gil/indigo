b8e67aed9343ca612ad0db0a8ba7e0aa4f449c6f
vic-gil <vicgilroman@gmail.com>
2021-03-04 16:14:42 -0600

    Se actualiza la entrada

--- single.php
+++ single.php
@@ -1,26 +1,22 @@
-<?php 
-$terms = wp_list_pluck( get_terms( 'ri-voto' ), 'term_id' );
-get_header(); 
-?>
+<?php
+/**
+ * Plantilla para las entradas
+ *
+ * @package Capital Media
+ * @subpackage Reporte Indigo
+ * @since Reporte Indigo 3.0.0
+ */
+get_header(); ?>
 <main>
 	<?php
-	if ( ! has_term($terms, 'ri-voto') ):
-		while ( have_posts() ) : the_post();
-			get_template_part( 'template-parts/sections/ri', 'general' );
-		endwhile; // End the loop.
-		Reporte_indigo_test::comment('Newsletter');
-		?>
-		<div class="content-max">
-			<div class="content">
-			<?php
-				Reporte_indigo_templates::componente_boletin( get_permalink( get_page_by_path( 'Newsletter' ) ) );
-			?>
-			</div>
-		</div>
-	<?php
-	else:
-		get_template_part( 'template-parts/sections/ri', 'voto' );
-	endif;
+	while ( have_posts() ) : the_post();
+		$voto = get_the_terms( get_the_ID(), 'ri-voto' );
+		if ( false !== $voto && ! is_wp_error( $voto ) ):
+			get_template_part( 'template-parts/single/ri', 'voto', [ 'terms' => $terms ] );
+		else:
+			get_template_part( 'template-parts/single/ri', 'general' );
+		endif;
+	endwhile; // End the loop.
 	?>
 </main>
 <?php get_footer(); ?>
