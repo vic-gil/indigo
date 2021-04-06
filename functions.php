0b434688de9fd560405ccd8286fd50fcd15442b3
vic-gil <vicgilroman@gmail.com>
2021-04-06 16:47:18 -0500

    Agregar async a scripts de plugins

--- functions.php
+++ functions.php
@@ -254,9 +254,9 @@ function reporte_indigo_scripts () {
   		wp_deregister_style('wordpress-popular-posts-css');
 
   		wp_dequeue_style( 'wp-block-library' );
-	    wp_dequeue_style( 'wp-block-library-theme' );
-	    wp_dequeue_style( 'wc-block-style' );
-	    wp_dequeue_style( 'wp-block-library' );
+	    	wp_dequeue_style( 'wp-block-library-theme' );
+	    	wp_dequeue_style( 'wc-block-style' );
+	    	wp_dequeue_style( 'wp-block-library' );
 
 	    if( ! is_single() ){
 	    	wp_dequeue_style('embedpress');
@@ -284,10 +284,15 @@ function reporte_indigo_scripts () {
 		wp_script_add_data( 'indigo', 'async', true );
 
 		wp_enqueue_script('bootstrap-min-js', 'https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.min.js', '', '5.0.0', true);
-		wp_script_add_data( 'bootstrap-min-js', 'defer', true );
+		wp_script_add_data( 'bootstrap-min-js', 'async', true );
 
 		wp_enqueue_style('swiper-min-css', 'https://cdnjs.cloudflare.com/ajax/libs/Swiper/4.5.0/css/swiper.min.css', [], '4.5.0', 'all' );
 		wp_enqueue_style('fontawesome-min-css', get_template_directory_uri() . '/assets/fonts/fontawesome/fontawesome.min.css', [], '1.0.0', 'all' );
+		
+		wp_script_add_data( 'hoverintent-js', 'async', true );
+		wp_script_add_data( 'admin-bar', 'async', true );
+		wp_script_add_data( 'embedpress-pdfobject', 'async', true );
+		wp_script_add_data( 'wpp-js', 'async', true );
 	}
 
 }
