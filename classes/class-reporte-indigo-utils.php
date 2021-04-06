92560f1401b42ca6bbb2e66a494cc0845be15f83
vic-gil <vicgilroman@gmail.com>
2021-04-06 14:49:41 -0500

    Añadir clase para utilerias

---
+++ classes/class-reporte-indigo-utils.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * Clase Reporte_Indigo_Utils
+ *
+ * Utilerias del tema
+ *
+ * @package Capital Media
+ * @subpackage Reporte Indigo
+ * @since Reporte Indigo 3.0.0
+ */
+
+if ( ! class_exists( 'Reporte_Indigo_Utils' ) ) {
+	
+	/**
+	 * Una clase que proveé diferentes metodos para funciones especificas de código
+	 */
+
+	class Reporte_Indigo_Utils{
+
+		/**
+		 * Compara si la hora actual está entre dos horarios y arroja un código
+		 *
+		 * @param Array $schedule 	Array de un horario
+		 *
+		 * @return string|bool El código del elemento multimedia o falso si no coincide
+		 */
+
+		public static function get_live_code($schedule) {
+			$code = false;
+			$weekday = current_time('w');
+
+			if( array_key_exists( $weekday, $schedule ) ) {
+				if( is_array( $schedule[$weekday] ) ) {
+					$current_time = current_time('His');
+					error_log($current_time);
+					foreach ( $schedule[$weekday] as $show ) {
+						$start = str_replace( ':', '', $show['start'] );
+						$end = str_replace( ':', '', $show['end'] );
+						
+						if( $start <= $current_time && $end >= $current_time ) {
+							$title = $show['title'];
+							preg_match('/embed\/([^"]+)"/', $show['embed'], $match);
+							$code = $match[1];
+							break;
+						}
+
+					}
+
+				} 
+			}
+
+			return $code;
+		}
+
+	}
+
+}
