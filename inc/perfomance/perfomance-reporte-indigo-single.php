1034d9e58515cd6a741e9d692b6d7a32d87399a8
vic-gil <vicgilroman@gmail.com>
2021-04-14 19:57:03 -0500

    Actualizar funciones

--- inc/perfomance/perfomance-reporte-indigo-single.php
+++ inc/perfomance/perfomance-reporte-indigo-single.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * Agrega la clase lazyload a las imagenes adjuntas
  *
@@ -33,27 +32,4 @@ function reporte_indigo_add_lazy_image($content){
 }
 
 add_filter('the_content','reporte_indigo_add_lazy_image');
-
-/**
- * ASe modifica la URL de las imagenes para conincidir con el CDN
- *
- * @param string  $content  Contenido de la entrada actual
- *
- * @return string El contenido modificado
- *
-**/
-
-function reporte_indigo_replace_url_image($content){
-	$origin = get_theme_mod( 'ri_images_original', FALSE );
-	$replace = get_theme_mod( 'ri_images_replace', FALSE );
-
-	if ( FALSE !== $origin && FALSE !== $replace ) {
-		$content = preg_replace("/(http|https):\/\/(?:.*?)\/wp-content\//i", "https://{$replace}/wp-content/", $content);
-	}
-
-	return $content;
-}
-
-add_filter('the_content','reporte_indigo_replace_url_image');
-
 ?>
